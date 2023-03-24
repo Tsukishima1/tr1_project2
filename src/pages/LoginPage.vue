3dbaefc8b41ba06dd9594d6299e8eff3e5c36c47
Tsukishima1 <1732124764@qq.com>
2023-03-24 22:50:46 +0800

    🎆修改登录页面样式布局

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,24 @@
 <template>
-    <label for="username">用户名</label>
-    <el-input v-model="username" placeholder="请输入用户名" id="username" />
-    <label for="password">密码</label>
-    <el-input type="password" show-password v-model="password" placeholder="请输入密码" id="password"/>
-    <br> <br>
-    <button class="login" @click="login">登录</button>
-    <button class="register" @click="register">注册</button><br><br>
-    <button @click="toHomePage()">切换到homepage</button>
+    <div class="box">
+    <div class="inputbox">
+        <div class="username">
+        <p for="username" class="animate__animated" :class="{ 'slideInLeft': isFocused1 }">用户名✨</p>
+        <el-input @focus="onFocus1" @blur="onBlur1" v-model="username" placeholder="请输入用户名" id="username" />
+        </div>
+        <div class="password">
+        <p for="password" class="animate__animated" :class="{ 'slideInLeft': isFocused2 }">密码✨</p>
+        <el-input @focus="onFocus2" @blur="onBlur2" type="password" show-password v-model="password" placeholder="请输入密码" id="password"/>
+        </div>
+        <div class="button">
+            <button class="login" @click="login">普通用户登录</button>
+            <el-divider/>
+            <button class="login adminlogin" @click="login">管理员登录</button>
+        </div>
+        </div>
+    </div>
+    <br>
+    <!-- <button class="register" @click="register">注册</button><br><br> -->
+    <!-- <button @click="toHomePage()">切换到homepage</button> -->
 </template>
     
 <script setup lang='ts'>
@@ -14,6 +26,22 @@
     import { useRouter } from 'vue-router';
     import { userRegister,userLogin } from '../http/api/user';
     const router = useRouter();
+    const isFocused1 = ref(false);
+    const isFocused2 = ref(false);
+
+    const onFocus1 = () => {
+        isFocused1.value = true;
+    }
+    const onBlur1 = () => {
+        isFocused1.value = false;
+    }
+
+    const onFocus2 = () => {
+        isFocused2.value = true;
+    }
+    const onBlur2 = () => {
+        isFocused2.value = false;
+    }
 
     function toHomePage() {
         router.push({name: 'homepage'});
@@ -40,8 +68,5 @@
     }
 </script>
     
-<style scoped>
-    .login {
-        margin-right: 10px;
-    }
+<style scope src="../assets/style/login.less" lang="less">
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      less: {
+        charset: false,
+        additionalData: '@import "./src/assets/style/global.less";',
+      },
+    },
+  },
 })
