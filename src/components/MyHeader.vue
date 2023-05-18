3086d573c15da5f9eac38e028ff646d9e61a3b94
Tsukishima1 <1732124764@qq.com>
2023-05-19 00:58:03 +0800

    style(header): 重写页头样式

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,49 +1,16 @@
 <template>
-    <div class="header">
-        <div class="left">
-            <span style="margin-left: 2rem;">Hello, {{ username }}</span>
-            <span class="admin_name" v-show="isAdmin">管理员</span>
-            <span class="antiadmin_name" v-show="!isAdmin">普通用户</span>
+    <div class="container">
+        <div class="header">
+            <span class="circle"></span>
+            <h3>✨Welcome✨</h3>
+            <h2 class="username">{{ username }}</h2>
         </div>
-        <div class="right">
-            <button @click="backtologin()">退出登录</button>
-        </div>
+        <div class="divider"></div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import { useRouter } from 'vue-router';
-    import { UserFilled } from '@element-plus/icons-vue'
-    const username = sessionStorage.getItem('username');
-    const router = useRouter();
-    let isAdmin=ref<boolean>(false);
-
-    function backtologin() {
-        ElMessageBox.confirm(
-            '嘿！你确定要退出当前帐号吗？🤔',
-            '确认信息',
-            {
-                confirmButtonText: '确认退出！',
-                cancelButtonText: '再等等',
-                type: 'warning',
-            }
-        ).then(()=>{
-            ElMessage({
-                type: 'success',
-                message: '登出成功！',
-            });
-            router.replace('/');
-        }).catch(()=>{
-            ElMessage({
-                type: 'info',
-                message: '已取消登出',
-            })
-        })
-    }
-
-    onMounted(() => {
-        isAdmin.value = username==='admin'?true:false;
-    })
+    const username = sessionStorage.getItem("username");
 </script>
 
 <style scoped src="@/assets/style/myheader.css"></style>
